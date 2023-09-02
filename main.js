78895a75996e8999f80051f7260cfac97e5e7947
Yshmeel <me@yshmeel.dev>
2023-09-02 09:28:58 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+navigator.serviceWorker.register('service-worker.js')
+    .then(() => {
+        console.log('Service worker has been registered');
+    }).catch(() => {
+        console.error('Something went wrong when attempting to register service worker');
+    });
